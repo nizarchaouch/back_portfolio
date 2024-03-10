8366378a39fcf715edde2c2efd0e9e2fb24f3c87
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-03-11 00:15:57 +0100

    add model recruteur

---
+++ models/recruteur.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const candSchema = new mongoose.Schema({
+  nomEntreprise: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  identifiant: {
+    type: String,
+    require: true,
+  },
+  secteur: {
+    type: String,
+    require: true,
+  },
+  nom: {
+    type: String,
+    require: true,
+  },
+  prenom: {
+    type: String,
+    require: true,
+  },
+  dateNais: {
+    type: Date,
+    require: true,
+  },
+  tel: {
+    type: Number,
+    require: true,
+  },
+  civilite: {
+    type: String,
+    require: true,
+  },
+  adress: {
+    type: String,
+    require: true,
+  },
+  mail: {
+    type: String,
+    unique: true,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  verifier: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("candidat", candSchema);
