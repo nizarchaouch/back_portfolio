66f04d984b2f0bfdcc12bc1d4e6c0abd426c2d3e
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-03-27 03:17:31 +0100

    add router show

--- routes/offerR.js
+++ routes/offerR.js
@@ -4,4 +4,6 @@ const offerController = require("../controllers/offerController");
 
 router.post("/add", offerController.add);
 
+router.get("/show", offerController.show);
+
 module.exports = router;
