3d04ee4493ee3e0ffa694dea82a54f73e550814d
Chaouch Nizar <chaouchnizar1@gmail.com>
2024-03-27 02:36:10 +0100

    create model offer

---
+++ models/offer.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const offerSchema = new mongoose.Schema({
+  nomEntreprise: {
+    type: String,
+    require: true,
+  },
+  titre: {
+    type: String,
+    require: true,
+  },
+  position: {
+    type: String,
+    require: true,
+  },
+  vacants: {
+    type: Number,
+    require: true,
+    default: 1,
+  },
+  typeOffer: {
+    type: String,
+    require: true,
+  },
+  experience: {
+    type: String,
+    require: true,
+  },
+  niveauCand: {
+    type: String,
+    require: true,
+  },
+  langue: {
+    type: String,
+    require: true,
+  },
+  description: {
+    type: String,
+    require: true,
+  },
+  exigence: {
+    type: String,
+    require: true,
+  },
+  date_creation: {
+    type: Date,
+    require: true,
+  },
+  date_expiration: {
+    type: Date,
+    require: true,
+  },
+  motCle: {
+    type: Array,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("offer", offerSchema);
